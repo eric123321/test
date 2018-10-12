e52e1b5aeddd03a411c18f02cf197a3226a9b7bd
Li Jun <770516200@qq.com>
2018-10-12 11:14:48 +0800

    add files

---
+++ header的副本.php
@@ -0,0 +1,57 @@
+ <html>
+     <head>
+        <title>PandaPark</title>
+        <link rel="stylesheet" type="text/css" href="css/panda.css">
+               
+     </head>
+     <body>
+        <header>
+            <div class="photo">
+                <img src='images/panda.png'>
+            </div>
+            <div class="eee">Chengdu Panda Park
+            </div>
+        </header>
+     
+         <div class="menu">
+             <ul>                 
+                <li><a href="home.php"> Home </a></li>
+                <li><a href="doctor.php"> Doctor </a>
+                    <ul>
+                        <li><a href="doctor.php">Table</a></li>
+                        <li><a href="doctor2.php">Diagram</a></li>
+                        <li><a href="relationship.php">Relationship</a></li>
+                    </ul>
+                </li>
+                <li><a href="caregiver.php"> Caregiver </a>
+                    <ul>
+                        <li><a href="caregiver.php">Table of food</a></li>
+                        <li><a href="caregiver2.php">Total of food</a></li>
+                    </ul>
+                 </li>
+                <li><a href="finance.php"> Finance </a>
+                    <ul>
+                        <li><a href="salary.php">salary</a></li>
+                        <li><a href="cost.php">cost</a>
+                            <ul>
+                                <li><a href="day.php">day</a></li>
+                                <li><a href="month.php">month</a></li>
+                                <li><a href="year.php">year</a></li>
+                            </ul>
+                        </li>
+                    </ul>    
+                </li>    
+                <li><a href="security.php"> Security </a></li>
+                <li><a href="administrator.php"> Administrator </a></li>
+                <li><a href="includes/logout.php"> logout </a></li>
+             </ul>
+         </div> 
+         <br>
+         
+       
+         
+         
+          
+           
+         
+         
